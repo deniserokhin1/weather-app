d3efdad662baa6c36d0e368d8fca93b40b9b25a9
Denis <oterohina@gmail.com>
2022-09-04 21:45:52 +0300

    Add Starter Pack

---
+++ webpack.config.js
@@ -0,0 +1,73 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+module.exports = {
+  context: path.resolve(__dirname, 'src'),
+  entry: {
+    main: './main.js',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[contenthash].js',
+    clean: true,
+  },
+  resolve: {
+    extensions: ['.js', '.json', '.png', '.css'],
+    alias: {
+      '@lib': path.resolve(__dirname, 'src/lib'),
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  mode: isProduction ? 'production' : 'development',
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+      minify: {
+        collapseWhitespace: isProduction,
+      },
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css',
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'src/favicon.ico'),
+          to: path.resolve(__dirname, 'dist'),
+        },
+      ],
+    }),
+  ],
+  optimization: {
+    minimizer: ['...', new CssMinimizerPlugin()],
+  },
+  devServer: {
+    port: 5000,
+  },
+  devtool: isProduction ? false : 'source-map',
+};
